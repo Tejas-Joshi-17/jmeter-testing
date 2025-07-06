0f411ece913d810d9d6a35acb92db8c5911a02f2
Tejas-Joshi-28 <tejasjoshi2804@gmail.com>
2025-07-06 15:03:15 +0530

    changes done only for jenkins

--- src/main/java/com/sarvatra/JmeterTestingApplication.java
+++ src/main/java/com/sarvatra/JmeterTestingApplication.java
@@ -1,13 +1,18 @@
 package com.sarvatra;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JmeterTestingApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(JmeterTestingApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(JmeterTestingApplication.class, args);
+//		SpringApplication.run(JmeterTestingApplication.class, args);
+		logger.info("This is project started successfully");
 	}
 
 }
