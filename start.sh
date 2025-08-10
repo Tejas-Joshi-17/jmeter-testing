aadc93113375c85f44a583838e30424243b16136
Tejas-Joshi-28 <tejasjoshi2804@gmail.com>
2025-08-10 20:22:24 +0530

    update dependency & starting script

--- start.sh
+++ start.sh
@@ -1,5 +1,14 @@
 rm -rf ./logs/*.*
 
+# Kill any running jmeter-testing process
+PID=$(ps -ef | grep "jmeter-testing" | grep -v grep | awk '{print $2}')
+if [[ -n $PID ]]; then
+  kill -9 "$PID"
+  echo "Already Running Process Stopped ..."
+else
+  echo "Ready to Build Project"
+fi
+
 source "/home/tejas-joshi/Documents/Software/Sdkman/bin/sdkman-init.sh"
 sdk use java 17.0.12-oracle
 sdk use gradle 8.10.2
@@ -13,4 +22,4 @@ cp -r ./docker-start.sh ./build/libs
 cp -r ./src/main/resources/application.properties ./build/libs
 cp -r ./src/main/resources/logback-spring.xml ./build/libs
 
-java -jar ./build/libs/jmeter-testing-1.0.1.jar
+java -jar -Dspring.application.name="jmeter-testing" ./build/libs/jmeter-testing-1.0.1.jar
